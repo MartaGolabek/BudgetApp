af24cf48d641596434fd9638d01e5d7262174141
Marta Golabek <mgolabek94@gmail.com>
2018-02-24 19:24:04 +0100

    Year select mockup

--- controller/app.js
+++ controller/app.js
@@ -2,8 +2,23 @@
 'use strict';
 
 angular.module('budgetApp', [])
-.controller("budgetController", function() {
+.controller("budgetController", ['$scope', function($scope) {
 
-});
+	// mockup for year select
+	// TODO: implement CRUD operations
+	$scope.data = {
+	    model: '1',
+	    years: [
+	      {id: '1', year: '2018'},
+	      {id: '2', year: '2019'},
+	      {id: '3', year: '2020'},
+	      {id: '3', year: '2021'}
+	    ]
+	   };
+
+	
+
+
+}]);
 
 })();
