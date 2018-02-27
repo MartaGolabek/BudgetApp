da07df544e59f71f797d12a5ea19d3298a7b0122
Marta Golabek <mgolabek94@gmail.com>
2018-02-27 20:24:36 +0100

    Budget App: add new entry

--- controller/app.js
+++ controller/app.js
@@ -16,7 +16,32 @@ angular.module('budgetApp', [])
 	    ]
 	   };
 
-	
+	   $scope.data2 = {
+	    model: '1',
+	    months: [
+	      {id: '1', month: 'January'},
+	      {id: '2', month: 'February'},
+	      {id: '3', month: 'March'},
+	      {id: '4', month: 'April'},
+	      {id: '5', month: 'May'},
+	      {id: '6', month: 'June'},
+	      {id: '7', month: 'July'},
+	      {id: '8', month: 'August'},
+	      {id: '9', month: 'September'},
+	      {id: '10', month: 'October'},
+	      {id: '11', month: 'November'},
+	      {id: '12', month: 'December'}
+	    ]
+	   };
+
+	   $scope.budgetValue = 1000;
+
+		
+		$scope.budgetEntity = {
+			year: '2018',
+			month: 'June',
+			budgetValue: 0
+		};	
 
 
 }]);
